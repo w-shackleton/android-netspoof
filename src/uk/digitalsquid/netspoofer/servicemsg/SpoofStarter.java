bb9fae332c667c3637427d8293f500a38ecd0226
William Shackleton <w.shackleton@gmail.com>
2011-07-02 22:03:22 +0100

    GPL'd code.

--- src/uk/digitalsquid/netspoofer/servicemsg/SpoofStarter.java
+++ src/uk/digitalsquid/netspoofer/servicemsg/SpoofStarter.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of Network Spoofer for Android.
+ * Network Spoofer - change and mess with webpages and the internet on
+ * other people's computers
+ * Copyright (C) 2011 Will Shackleton
+ *
+ * Network Spoofer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Network Spoofer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Network Spoofer, in the file COPYING.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package uk.digitalsquid.netspoofer.servicemsg;
 
 import uk.digitalsquid.netspoofer.spoofs.SpoofData;
