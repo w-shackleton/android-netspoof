ea0394b96875e2b6ec130566e4157864557e29e3
William Shackleton <w.shackleton@gmail.com>
2011-10-23 01:26:28 +0100

    Release 0.8

--- src/uk/digitalsquid/netspoofer/config/Config.java
+++ src/uk/digitalsquid/netspoofer/config/Config.java
@@ -30,8 +30,8 @@ public interface Config extends LogConf {
 	public static final String DEB_IMG = "img/debian.img";
 	public static final String DEB_IMG_GZ = "img/debian.img.gz";
 	public static final String DEB_VERSION_FILE = "img/version";
-	public static final String SF_DEB_IMG_URL = "http://sourceforge.net/projects/netspoof/files/debian-images/debian-0.7.img.gz/download";
-	public static final String SF_DEB_IMG_URL_NOZIP = "http://sourceforge.net/projects/netspoof/files/debian-images/debian-0.7.img/download";
+	public static final String SF_DEB_IMG_URL = "http://sourceforge.net/projects/netspoof/files/debian-images/debian-0.7.1.img.gz/download";
+	public static final String SF_DEB_IMG_URL_NOZIP = "http://sourceforge.net/projects/netspoof/files/debian-images/debian-0.7.1.img/download";
 	public static final int DEB_IMG_URL_SIZE = 440320000;
-	public static final int DEB_IMG_URL_VERSION = 9;
+	public static final int DEB_IMG_URL_VERSION = 10;
 }
