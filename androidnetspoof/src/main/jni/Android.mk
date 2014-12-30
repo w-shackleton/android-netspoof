fbb006b8cc0436258bfa3128cc129a1d441323cf
Will Shackleton <w.shackleton@gmail.com>
2014-12-30 15:30:05 +0000

    Included arpspoof JNI code from dSploit

--- androidnetspoof/src/main/jni/Android.mk
+++ androidnetspoof/src/main/jni/Android.mk
@@ -2,6 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+include $(LOCAL_PATH)/arpspoof/Android.mk
+include $(LOCAL_PATH)/libpcap/Android.mk
+include $(LOCAL_PATH)/libnet/Android.mk
+
 LOCAL_MODULE    := netspooflib
 LOCAL_SRC_FILES := uk_digitalsquid_netspoofer_JNI.c
 
---
+++ androidnetspoof/src/main/jni/Application.mk
@@ -0,0 +1,3 @@
+APP_STL := gnustl_static
+APP_PLATFORM := android-9
+NDK_TOOLCHAIN_VERSION := 4.8
