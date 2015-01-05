2b0703cf314b5af8c91350987bfbeec936bc00e6
Will Shackleton <w.shackleton@gmail.com>
2015-01-05 14:39:10 +0000

    Attempted compilation on 64-bit platforms. Did not work.

--- androidnetspoof/src/main/jni/Application.mk
+++ androidnetspoof/src/main/jni/Application.mk
@@ -1,4 +1,4 @@
 APP_STL := gnustl_static
 APP_PLATFORM := android-9
 NDK_TOOLCHAIN_VERSION := 4.8
-APP_ABI := x86 armeabi armeabi-v7a mips
+APP_ABI := armeabi armeabi-v7a mips x86 # x86_64 mips64 arm64-v8a don't work yet
