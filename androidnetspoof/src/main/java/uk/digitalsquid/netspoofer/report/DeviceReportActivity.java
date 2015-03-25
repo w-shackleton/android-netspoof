0db91cf50ef161d3575d0d1ff23b823cbf6d145d
Will Shackleton <w.shackleton@gmail.com>
2015-03-25 17:33:17 +0000

    Finished off report GUI

--- androidnetspoof/src/main/java/uk/digitalsquid/netspoofer/report/DeviceReportActivity.java
+++ androidnetspoof/src/main/java/uk/digitalsquid/netspoofer/report/DeviceReportActivity.java
@@ -21,9 +21,14 @@
 
 package uk.digitalsquid.netspoofer.report;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.FileProvider;
@@ -35,6 +40,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -137,8 +143,65 @@ public class DeviceReportActivity extends FragmentActivity {
                     shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
                     shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     shareIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { "nsreport@digitalsquid.co.uk" });
-                    startActivity(Intent.createChooser(shareIntent,
-                            getActivity().getString(R.string.send_via)));
+                    startActivityForResult(Intent.createChooser(shareIntent,
+                            getActivity().getString(R.string.send_via)), 1);
+                    break;
+            }
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            switch (requestCode) {
+                case 1: // Email sent correctly
+                    // Move on to final bit of UI
+                    SuccessFragment fragment = new SuccessFragment();
+                    Bundle args = new Bundle();
+                    args.putString("code", code);
+                    fragment.setArguments(args);
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container, fragment)
+                            .commit();
+                    break;
+            }
+        }
+    }
+
+    public static class SuccessFragment extends Fragment implements View.OnClickListener {
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_device_report_prompt, container, false);
+
+            rootView.findViewById(R.id.copy).setOnClickListener(this);
+            rootView.findViewById(R.id.submit).setOnClickListener(this);
+
+            TextView codeView = (TextView) rootView.findViewById(R.id.code);
+            codeView.setText(getCode());
+            return rootView;
+        }
+
+        private String getCode() {
+            return getArguments() != null ?
+                    getArguments().getString("code") :
+                    "correct-horse-battery-staple";
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.copy:
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                        android.content.ClipboardManager cm = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                        cm.setPrimaryClip(ClipData.newPlainText("Network Spoofer report code", getCode()));
+                    } else {
+                        android.text.ClipboardManager cm = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                        cm.setText(getCode());
+                    }
+                    Toast.makeText(getActivity(), "Copied code to clipboard", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.submit:
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/w-shackleton/android-netspoof/issues/"));
+                    startActivity(intent);
                     break;
             }
         }
